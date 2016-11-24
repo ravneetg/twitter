d84f9b31da401c9bc7756cb6ae1de14efcdb2829
blue <blue@blue.home.com>
2016-11-24 16:48:56 -0700

    updated files

---
+++ extract_tweets_restful.py
@@ -0,0 +1,61 @@
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+import json
+from pandas.io.json import json_normalize
+from kafka import KafkaProducer
+
+#producer = KafkaProducer(bootstrap_servers='localhost:9092')
+#producer.send('twitterstream', b'some_message_bytes')
+
+##########################################################################
+num_of_tweets=1 #enter number of tweets x 100 that need to be extracted
+#### e.g. value of 3 would return max of 300 tweets
+##########################################################################
+
+#-----------------------------------------------------------------------
+# load  API credentials
+#-----------------------------------------------------------------------
+config = {}
+execfile("config.py", config)
+
+#-----------------------------------------------------------------------
+# create twitter API object
+#-----------------------------------------------------------------------
+twitter = Twitter(
+		        auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
+
+#create a producer to write json messages to kafka
+producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+#iterator=twitter.search.tweets(q='#KevinHartWhatNow', result_type='recent', lang='en', count=10)
+
+i=0
+iterator=twitter.search.tweets(q = "#DrStrange", result_type='recent', lang='en', count = 100)
+n_max = float('+inf')
+n_min = float('-inf')
+
+for i in range(num_of_tweets):
+	print "Search complete (%.3f seconds)" % (iterator["search_metadata"]["completed_in"])
+	#json_normalize(iterator)[["text"]]
+	count=0
+	for tweet in iterator["statuses"]:
+		count += 1
+		#jsontweet = json.loads(tweet)
+		tweet_id=tweet['id']
+		#print tweet['id']
+		if tweet_id < n_max:
+			min_id=tweet_id
+			n_max = tweet_id
+		if tweet_id > n_min:
+			max_id=tweet_id
+			n_min = tweet_id
+
+		producer.send('Twitter', tweet)
+		###print json.dumps(tweet, indent=4)
+	#print "#####################Minimum ID#################### %s" % min_id
+	i += 1
+	print count
+	iterator=twitter.search.tweets(q = "#DrStrange", result_type='recent', lang='en', count = 100, max_id=min_id)
+	#iterator=twitter.search.tweets(q = "#DrStrange", result_type='recent', lang='en', count = 2000, since_id=max_id)
+
+
+#print "Result: %s:%s : %s" % (tweet["created_at"],tweet["user"]["screen_name"],tweet["text"])
---
+++ extract_tweets_streaming.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import, print_function
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from kafka import KafkaProducer
+#from kafka.client import KafkaClient
+from kafka.client import SimpleClient
+from kafka.consumer import SimpleConsumer
+from kafka.producer import SimpleProducer
+
+#***********producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+client = SimpleClient("localhost:9092")
+producer = SimpleProducer(client)
+
+consumer_key = ' ' #'aybpmREJAzUkbrF2f0cWg'#eWkgf0izE2qtN8Ftk5yrVpaaI
+consumer_secret = ' '#BYYnkSEDx463mGzIxjSifxfXN6V1ggpfJaGBKlhRpUMuQ02lBX
+access_token = ' '#1355650081-Mq5jok7mbcrIbTpqZPcMHgWjcymqSrG1kVaut39
+access_token_secret = ' '#QovqxQnw0hSPrKwFIYLWct3Zv4MeGMash66IaOoFyXNWs
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self, data):
+        producer.send_messages('Twitter', str(data))
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(track=['#android'],languages=["en"])
